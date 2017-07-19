87f5df29a2b52d928f8231c98fca5e81bd60cee8
Dag Wieers <dag@wieers.com>
2017-07-19 04:22:43 +0200

    Small improvements to the rdesktop wrapper

---
+++ rdesktop.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Connecting to ${@/computer/192.168.0.}"
+#exec rdesktop -u ictadmin -p - -k nl-be -r disk:map=share/ -g 1024x768 ${@/computer/192.168.0.}
+exec rdesktop -u ictadmin -k nl-be -r disk:map=share/ -g 1024x768 ${@/computer/192.168.0.} &
