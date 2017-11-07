425025f9aa8ec62a039de0ab84439bff604eeb0d
Dag Wieers <dag@wieers.com>
2017-11-07 17:37:05 +0100

    Increase default screen size

--- rdesktop.sh
+++ rdesktop.sh
@@ -2,4 +2,4 @@
 
 echo "Connecting to ${@/computer/192.168.0.}"
 #exec rdesktop -u ictadmin -p - -k nl-be -r disk:map=share/ -g 1024x768 ${@/computer/192.168.0.}
-exec rdesktop -u ictadmin -k nl-be -r disk:map=share/ -g 1024x768 ${@/computer/192.168.0.} &
+exec rdesktop -u ictadmin -k nl-be -r disk:map=share/ -g 1600x900 ${@/computer/192.168.0.} &
