9c756bcd9477b9f708606835539e8e55eebdb443
Dag Wieers <dag@wieers.com>
2019-03-26 14:56:00 +0100

    Improve running rdesktop

--- rdesktop.sh
+++ rdesktop.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-echo "Connecting to ${@/computer/192.168.0.}"
-#exec rdesktop -u ictadmin -p - -k nl-be -r disk:map=share/ -g 1024x768 ${@/computer/192.168.0.}
-exec rdesktop -u ictadmin -k nl-be -r disk:map=share/ -g 1600x900 ${@/computer/192.168.0.} &
+target=$1
+
+if ! host $target &>/dev/null; then
+    target=${target/computer/192.168.0.}
+fi
+
+echo "Connecting to $target"
+#exec rdesktop -u ictadmin -p - -k nl-be -r disk:map=share/ -g 1024x768 $target
+exec rdesktop -u ictadmin -k nl-be -r disk:map=share/ -g 1600x900 $target &
