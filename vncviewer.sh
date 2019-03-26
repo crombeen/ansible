76602d871eddfba79b93ce8e4f3c0a69a30dfedf
Dag Wieers <dag@wieers.com>
2019-03-26 14:52:26 +0100

    Improve vncviewer

--- vncviewer.sh
+++ vncviewer.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-echo "Connecting to ${@/computer/192.168.0.}"
-exec vncviewer ${@/computer/192.168.0.} -CompressLevel=2 -QualityLevel=1 -PreferredEncoding=ZRLE &
+target=$1
+
+if ! host $target &>/dev/null; then
+    target=${target/computer/192.168.0.}
+fi
+
+echo "Connecting to $target"
+exec vncviewer $target -CompressLevel=2 -QualityLevel=1 -PreferredEncoding=ZRLE &
